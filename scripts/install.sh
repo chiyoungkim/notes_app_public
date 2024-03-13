0f465f7a53f7142bac9cdf8e8110bbe375aa3fc3
Chiyoung Kim <chiyoung@Chiyoungs-MBP.MG8702>
2024-03-13 16:32:24 -0400

    aws stuff

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+source /home/ec2-user/.bash_profile
+
+# Install project dependencies
+cd /var/www/html/
+npm install
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,7 +5,3 @@ sudo yum install -y nodejs
 
 # Create the deployment directory if it doesn't exist
 sudo mkdir -p /var/www/html/
-
-# Install project dependencies
-cd /var/www/html/
-npm install
