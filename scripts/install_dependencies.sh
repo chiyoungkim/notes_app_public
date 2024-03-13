d25f651527e1ce0439d7661c353e3a9822e26ce6
Chiyoung Kim <chiyoung@Chiyoungs-MBP.MG8702>
2024-03-13 14:55:40 -0400

    yum

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@ source /home/ec2-user/.bash_profile
 
 # Install Node.js
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo yum install -y nodejs
 
 # Install project dependencies
 cd /var/www/html/
